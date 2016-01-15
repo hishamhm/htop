95822f3295aecfc8ad4552bcaf4bcd417b87c353
Explorer09 <explorer09@gmail.com>
2016-01-15 10:17:50 +0800

    Fix prevItemSums[0] value error.
    (Stupid me for forgetting to delete the line when changing code in commit 5c18866ee6b8baeba984f1c54afefe3a9af44efc )

--- Meter.c
+++ Meter.c
@@ -421,7 +421,6 @@ static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
       double *prevItemSums = data->prevItemSums;
       double *currentItemSums = data->currentItemSums;
       
-      currentItemSums[0] = this->values[0];
       for (int i = 0; i < items; i++) {
          prevItemSums[i] = currentItemSums[i];
          currentItemSums[i] = ((i > 0) ? currentItemSums[i-1] : 0.0) + this->values[i];
