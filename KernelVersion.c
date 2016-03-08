39d90165cfb8672f9f6857b2dd0f3889af71bb01
jjungo <j.jungo@gmail.com>
2016-03-08 08:34:23 +0100

    issue #434 mistake on headers

--- KernelVersion.c
+++ KernelVersion.c
@@ -1,5 +1,5 @@
 /*
-htop - UptimeMeter.c
+htop - KernelVersion.c
 (C) 2004-2011 Hisham H. Muhammad
 Released under the GNU GPL, see the COPYING file
 in the source distribution for its full text.
--- KernelVersion.h
+++ KernelVersion.h
@@ -3,7 +3,7 @@
 #ifndef HEADER_KernelVersion
 #define HEADER_KernelVersion
 /*
-htop - UptimeMeter.h
+htop - KernelVersion.h
 (C) 2004-2011 Hisham H. Muhammad
 Released under the GNU GPL, see the COPYING file
 in the source distribution for its full text.
