13fc57b35e1ca31c3f8fee562e226fad6c6efc19
Guy M. Broome <thrirhrafnir@gmail.com>
2017-11-16 18:56:01 +0000

    Initial setup of Solaris platform directory

---
+++ solaris/SolarisProcess.c
@@ -0,0 +1,33 @@
+/*
+htop - UnsupportedProcess.c
+(C) 2015 Hisham H. Muhammad
+Released under the GNU GPL, see the COPYING file
+in the source distribution for its full text.
+*/
+
+#include "Process.h"
+#include "UnsupportedProcess.h"
+#include <stdlib.h>
+
+/*{
+#include "Settings.h"
+
+#define Process_delete UnsupportedProcess_delete
+
+}*/
+
+Process* UnsupportedProcess_new(Settings* settings) {
+   Process* this = xCalloc(1, sizeof(Process));
+   Object_setClass(this, Class(Process));
+   Process_init(this, settings);
+   return this;
+}
+
+void UnsupportedProcess_delete(Object* cast) {
+   Process* this = (Process*) cast;
+   Object_setClass(this, Class(Process));
+   Process_done((Process*)cast);
+   // free platform-specific fields here
+   free(this);
+}
+
