0c1f538735c3a755b14e4fec57f24d04b033f23b
Explorer09 <explorer09@gmail.com>
2018-04-13 18:40:05 +0800

    Rename some local variables in GraphMeterMode_draw()
    Rename "i" to "index". ("i" is not a loop counter here and so worth
    renaming.)
    Rename "k" to "col".
    
    Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

--- Meter.c
+++ Meter.c
@@ -413,15 +413,15 @@ static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
       data->values[GRAPH_NUM_RECORDS - 1] = value;
    }
    
-   int i = GRAPH_NUM_RECORDS - (w*2) + 2, k = 0;
-   if (i < 0) {
-      k = -i/2;
-      i = 0;
+   int index = GRAPH_NUM_RECORDS - (w * 2) + 2, col = 0;
+   if (index < 0) {
+      col = -index / 2;
+      index = 0;
    }
-   for (; i < GRAPH_NUM_RECORDS - 1; i+=2, k++) {
+   for (; index < GRAPH_NUM_RECORDS - 1; index += 2, col++) {
       int pix = GraphMeterMode_pixPerRow * GRAPH_HEIGHT;
-      int v1 = CLAMP((int) lround(data->values[i] * pix), 1, pix);
-      int v2 = CLAMP((int) lround(data->values[i+1] * pix), 1, pix);
+      int v1 = CLAMP((int) lround(data->values[index] * pix), 1, pix);
+      int v2 = CLAMP((int) lround(data->values[index + 1] * pix), 1, pix);
 
       int colorIdx = GRAPH_1;
       for (int line = 0; line < GRAPH_HEIGHT; line++) {
@@ -429,7 +429,7 @@ static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
          int line2 = CLAMP(v2 - (GraphMeterMode_pixPerRow * (GRAPH_HEIGHT - 1 - line)), 0, GraphMeterMode_pixPerRow);
 
          attrset(CRT_colors[colorIdx]);
-         mvaddstr(y+line, x+k, GraphMeterMode_dots[line1 * (GraphMeterMode_pixPerRow + 1) + line2]);
+         mvaddstr(y+line, x+col, GraphMeterMode_dots[line1 * (GraphMeterMode_pixPerRow + 1) + line2]);
          colorIdx = GRAPH_2;
       }
    }
